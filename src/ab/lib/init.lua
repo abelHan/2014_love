dccf04b24f5896e8404d46407faeb577b69ccc06
韩帅宾(幻影) <hansb@youzu.com>
2016-09-14 11:38:13 +0800

    fucntion class

--- src/ab/lib/init.lua
+++ src/ab/lib/init.lua
@@ -3,7 +3,7 @@
 -- 初始化
 
 local lib = {}
-lib.util = require("src.ab.lib.util")
-
+lib.util 		= require("src.ab.lib.util")
+lib.middleclass = require("src.ab.lib.middleclass.middleclass")
 
 return lib
